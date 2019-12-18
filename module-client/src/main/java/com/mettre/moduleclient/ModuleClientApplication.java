82ffee48531b8da132013368c161ae9942569a24
Mettre <1052461392@qq.com>
2019-12-18 21:20:08 +0800

    框架搭建

--- module-client/src/main/java/com/mettre/moduleclient/ModuleClientApplication.java
+++ module-client/src/main/java/com/mettre/moduleclient/ModuleClientApplication.java
@@ -2,11 +2,11 @@ package com.mettre.moduleclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@EnableEurekaServer
+@EnableEurekaClient
 @SpringBootApplication
 @RestController
 public class ModuleClientApplication {
